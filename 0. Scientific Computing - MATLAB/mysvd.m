5886c231b2468f08d145fc1d38fa788815216337
HoMoonKi <hewenqi96@gmail.com>
2019-01-20 17:24:47 -0500

    update

--- 0. Scientific Computing - MATLAB/mysvd.m
+++ 0. Scientific Computing - MATLAB/mysvd.m
@@ -4,20 +4,20 @@ function [U,S,V] = mysvd(A)
     tan = (A(2,1) - A(1,2)) / (A(1,1) + A(2,2));
     c = 1 / norm([1 tan], 2);
     s = tan / norm([1 tan], 2);
-    Rot = [c s; -s c];
+    rot = [c s; -s c];
     % Diagonalize
-    B = Rot * A;
+    B = rot * A;
     b = (B(2,2) - B(1,1))/ B(1,2);
     r = roots([1, b, -1]);
     tan = r(1);
     c = 1 / norm([1 tan], 2);
     s = tan / norm([1 tan], 2);
-    lRot = [c -s; s c];
-    rRot = [c s; -s c];
+    lrot = [c -s; s c];
+    rrot = [c s; -s c];
     % Combining two routines
-    U = Rot' * lRot';
-    S = lRot * B * rRot;
-    V = rRot;
+    U = rot' * lrot';
+    S = lrot * B * rrot;
+    V = rrot;
     % Correct the signs of singular values
     for i = 1:2
         if S(i,i) < 0
