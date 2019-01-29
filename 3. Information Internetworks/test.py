0c9d1ed8794458087b2fd73c636403043571db8c
Wenqi He <hewenqi96@gmail.com>
2019-01-29 15:36:21 -0500

    Update test.py

--- 3. Information Internetworks/test.py
+++ 3. Information Internetworks/test.py
@@ -4,7 +4,7 @@ e_ = self.target.r_ - self.missile.r_
 if self.t > self.missile.guidance_delay:
     prev_e_ = np.array([0,0,0])
 
-    # e_ will point to the projected collision location
+    # e_ will converge to the vector pointing to the eventual collision location
     while np.linalg.norm(e_) - np.linalg.norm(prev_e_) > 1.0:
         prev_e_ = e_
         ete_impact = np.linalg.norm(e_) / np.dot(self.missile.v_, normalize(e_))
@@ -14,4 +14,4 @@ if self.t > self.missile.guidance_delay:
     # Offset the velocity perpendicular to e_ to prevent drifting
     dir_ = normalize(e_)
     perp_v_ = self.missile.v_ - np.dot(self.missile.v_, dir_) * dir_
-    e_ = e_ - perp_v_ * np.linalg.norm(e_) / np.linalg.norm(self.missile.v_)
+    e_ = e_ - perp_v_ * np.linalg.norm(e_) / np.linalg.norm(self.missile.v_
