06b1df46deac8186712a2a04129a1bad33ef0b50
Wenqi He <hewenqi96@gmail.com>
2019-03-24 21:00:47 -0400

    Update freShavocado.m

--- 0. Modeling and Simulation/MATLAB/freShavocado.m
+++ 0. Modeling and Simulation/MATLAB/freShavocado.m
@@ -1,15 +1,15 @@
 function out = freShavocado(in)
-    remaining = in;
-    price = 0;
-    num = 0;
-    while  ~ isempty(remaining)
-        [token, remaining] = strtok(remaining);
-        if token(1) == '#'
-            num = str2num(token(2:end));
-        elseif token(1) == '$'
-            price = str2num(token(2:end));
-        end
+remaining = in;
+price = 0;
+num = 0;
+while  ~ isempty(remaining)
+    [token, remaining] = strtok(remaining);
+    if token(1) == '#'
+        num = str2num(token(2:end));
+    elseif token(1) == '$'
+        price = str2num(token(2:end));
     end
-    out = sprintf('I need $%.2f to buy fresh avocados.', price .* num);
+end
+out = sprintf('I need $%.2f to buy fresh avocados.', price .* num);
 end
 
